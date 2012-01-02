e5dfc6544fceb94af5fc5773875261f060b733b2
Donny <donny.redmond@gmail.com>
2012-01-01 20:47:34 -0600

    Added debugConsole class and started adding code for particles.

---
+++ HTML5Particle/js/debugConsole.js
@@ -0,0 +1,25 @@
+﻿var debugConsole = (function () {
+    var e = null;
+
+    this.init = function (id) {
+        e = document.getElementById(id);
+    };
+
+    this.clear = function () {
+        if (e && e.constructor == HTMLDivElement) {
+            e.innerHTML = '';
+        }
+    };
+
+    this.write = function (message) {
+        if (e && e.constructor == HTMLDivElement) {
+            e.innerHTML += message + '<br />';
+        }
+    };
+
+    return {
+        init: init,
+        write: write,
+        clear: clear
+    };
+})();
--- HTML5Particle/js/particle.js
+++ HTML5Particle/js/particle.js
@@ -1,1 +1,23 @@
-﻿
+﻿function Particle () {
+    var location = { x: 0, y: 0 };
+    var direction = { x: 0, y: 0 };
+    var energy = 100;
+
+    // Set the current location of the particle
+    this.setLocation = function (x, y) {
+        location.x = x;
+        location.y = y;
+    };
+
+    this.debug = function () {
+        return location.x + ', ' + location.y;
+    };
+
+    this.update = function() {
+
+    };
+
+    this.draw = function(canvas, img) {
+        
+    };
+};
