b3df644a9d2d15a3fded0984131cc6bda3b7d41b
Donny <donny.redmond@gmail.com>
2012-01-02 23:31:32 -0600

    Added code to animate particles, but the delta time has a bug causing particles to move to slowely.

--- HTML5Particle/js/emitter.js
+++ HTML5Particle/js/emitter.js
@@ -1,1 +1,61 @@
-﻿
+﻿function Emitter () {
+    var particles = [];
+    var image = null;
+    var lastFrameTime = 0;
+
+    this.init = function (img) {
+        for (var i = 0; i < 100; i++) {
+            var x = Math.random() * 801;
+            var y = Math.random() * 601;
+
+            var p = new Particle();
+            p.setLocation(x, y);
+            particles.push(p);
+        }
+
+        lastFrameTime = new Date().getTime();
+    };
+
+    this.update = function () {
+        var currentFrameTime = new Date().getTime();
+        var i, x, y;
+
+        if (lastFrameTime == 0) {
+            lastFrameTime = currentFrameTime;
+        }
+
+        var delta = (currentFrameTime - lastFrameTime) / 1000;
+        
+        if (delta <= 0)
+            return;
+        
+        lastFrameTime = new Date().getTime();
+
+        for (i = particles.length - 1; i >= 0; i--) {
+            particles[i].update(delta);
+
+            if (particles[i].dead()) {
+
+                // Create new particle.
+                particles[i] = new Particle();
+                x = Math.random() * 801;
+                y = Math.random() * 601;
+                particles[i].setLocation(x, y);
+            }
+        }
+    };
+
+    this.draw = function (ctx) {
+        for (var i in particles) {
+            particles[i].draw(ctx, image);
+        }
+    };
+
+    this.done = function() {
+        if (particles.length == 0) {
+            return true;
+        }
+
+        return false;
+    };
+};
--- HTML5Particle/js/particle.js
+++ HTML5Particle/js/particle.js
@@ -1,6 +1,33 @@
-﻿function Particle () {
-    var location = { x: 0, y: 0 };
-    var direction = { x: 0, y: 0 };
+﻿// Particle Types (This will be used later)
+var particleType = { Smoke: 0, Fire: 1, Hair: 2 };
+
+// Define our vector and it's operations
+function Vector () {
+    this.x = 0;
+    this.y = 0;
+
+    this.add = function (vector) {
+        if (vector && vector.constructor !== Vector)
+            return;
+        
+        this.x += vector.x;
+        this.y += vector.y;
+    };
+
+    this.mult = function(scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+    };
+}
+
+function Particle (ptype) {
+    var location = new Vector();
+    var velocity = new Vector();
+    
+    velocity.x = (Math.random() * 10) - 1;
+    velocity.y = (Math.random() * 10) - 1;
+    
+    this.type = ptype;
     var energy = 100;
 
     // Set the current location of the particle
@@ -13,11 +40,28 @@
         return 'Loc: ' + location.x + ', ' + location.y;
     };
 
-    this.update = function() {
-
+    this.update = function (delta) {
+        energy -= 1 * delta;
+        velocity.mult(delta);
+        location.add(velocity);
     };
 
-    this.draw = function(canvas, img) {
-        
+    this.draw = function (ctx, img) {
+        var alpha = energy / 100;
+        ctx.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')';
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(location.x, location.y, 2, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    };
+
+    this.dead = function() {
+        if (energy > 0) {
+            return false;
+        }
+
+        return true;
     };
 };
